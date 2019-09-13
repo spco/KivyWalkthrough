7353ef080ef7309236a97ff1578f8e4d44c9ba2e
Sam Cox <sam.cox@nottingham.ac.uk>
2019-09-13 15:34:09 +0100

    (2) This example introduces Screens, AnchorLayout, and size_hint.

--- myApp.py
+++ myApp.py
@@ -1,6 +1,15 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MainScreen(Screen):
+    pass
+
+
+class SettingsScreen(Screen):
+    pass
 
 
 class MainWindow(BoxLayout):
@@ -9,8 +18,12 @@ class MainWindow(BoxLayout):
 
 class ProtoApp(App):
     def build(self):
-        return MainWindow()
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(SettingsScreen(name='settings'))
+        return sm
 
 
 if __name__ == "__main__":
     ProtoApp().run()
+
